fda14a6bd28deef117b50ca00caf289577c3998d
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-09-23 14:08:25 -0700

    updated name of home page file

---
+++ game.js
@@ -0,0 +1,25 @@
+// global question array
+// array = json file?
+
+// global score value
+score = 0
+
+document.getElementById('main') = function() {
+
+}
+
+function pickQuestion(array) {
+ // pick a question from the array and set up the problem and answer on the page
+}
+
+function checkAnswer() {
+ // return the value of the selected item (radio button) and check it against the hidden value (correct answer)
+}
+
+function updateScore() {
+//when a question is correct, update the score total
+}
+
+
+
+
