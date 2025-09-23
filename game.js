bb4f3748c486e5d61cf6bc5688e9fc2674b5bb9a
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-09-23 14:48:46 -0700

    added prelim code to build the page

--- game.js
+++ game.js
@@ -1,19 +1,15 @@
 // global question array
+
+const { createElement } = require("react");
+
 // array = json file?
-object = loadArray("file-name");
+let object = loadArray("file-name");
 
 // global score value
-score = 0
-progress = 0
+let currentScore = 0
+let progress = 0
 
-document.getElementById('main') = function() {
-    pickQuestion(object);
-
-}
-
-document.getElementById('button').onclick = function() {
-
-}
+let correctAnswer = null;
 
 function loadArray(name) {
     // todo - convert json file to object
@@ -33,11 +29,47 @@ function updateScore() {
 //when a question is correct, update the score total
 }
 
-function progress(object) {
+function updateProgress(object) {
     // display number x of totalArrayLength
 }
 
 function updateObject(object) {
     // update the array so that you don't get a duplicate question
 }
+
+function buildPage() {
+    main = document.getElementById("main");
+    qa = pickQuestion(object);
+    
+    //clear out main in case this is a new question
+    main.textContent = ""
+
+    //add new children to main based on the question we picked
+    question = createElement(h1);
+    main.addChild(question);
+    question.id = "question"
+
+    scoreDisplay = createElement(p);
+    main.addChild(scoreDisplay)
+    scoreDisplay.textContent = currentScore;
+    scoreDisplay.id = "score"
+
+    progressDisplay = createElement(p);
+    main.addChild(progressDisplay);
+    progressDisplay.textContent = `${progress} / ${object.length}`
+    progress.id = "progress"
+    
+    for (item of qa.answers) {
+        //add radio button
+        //add text for radio button
+        //add value for radio button
+    }
+    
+
+    correctAnswer = qa.correctAnswer;
+}
+
+window.addEventListener('load', () => {
+    buildPage();
+})
 
